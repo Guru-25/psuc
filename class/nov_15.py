443bda4f4d38d26300e9e372de51e0ef1c1e20e4
Guru <gururaja.spr@gmail.com>
2022-11-15 21:24:14 +0530

    Update nov_15.py

--- class/nov_15.py
+++ class/nov_15.py
@@ -1,3 +1,11 @@
+# summation of n numbers
+
+n=int(input())
+sum1=0
+for i in range(n+1):
+    sum1=sum1+i
+print(sum1)
+
 # Fibonacci series of n numbers
 
 n=int(input())
@@ -17,3 +25,30 @@ else:
         x=y
         y=z
     print(*fib)
+    
+# check perfect number or not
+
+n=int(input())
+sum1=0
+for i in range(1,n):
+    if n%i==0:
+        sum1=sum1+i
+if sum1==n:
+    print("perfect number")
+else:
+    print("not a perfect number")
+    
+# check whether prime number or not
+
+n=int(input())
+if n>1:
+    for i in range(2,n):
+        if n%i==0:
+            print("not a prime number")
+            break
+    else:
+        print("prime number")
+else:
+    print("not a prime number")
+    
+    
